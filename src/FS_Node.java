54a9a646b96e62a4c6deb1552801826d84698b3d
Dantas58 <pedrodantas8@gmail.com>
2023-11-18 19:33:20 +0000

    Cenas por testar

--- src/FS_Node.java
+++ src/FS_Node.java
@@ -1,12 +1,14 @@
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.net.SocketException;
 import java.util.*;
 import java.io.*;
 
-
 public class FS_Node {
-    
-    private int TCP_Port;
+
+    // private final int TCP_Port = 9091;
     private String directory;
 
     private String server_address;
@@ -17,86 +19,26 @@ public class FS_Node {
     private ObjectInputStream in;
     private ObjectOutputStream out;
 
-    //private  String node_address;
+    private Socket tcp_socket;
+    private DatagramSocket udp_socket;
 
-    public FS_Node(int server_port, String server_address, String directory){
-        
-        this.TCP_Port = 9091;;
+    public FS_Node(int server_port, String server_address, String directory) {
+
         this.server_address = server_address;
         this.server_port = server_port;
         this.directory = directory;
-        //this.node_address = InetAddress.getLocalHost().getHostAddress(); // + ":" + String.valueOf(TCP_Port);
+        // tcp_socket = new Socket(server_address, server_port);
+        // this.node_address = InetAddress.getLocalHost().getHostAddress(); // + ":" +
+        // String.valueOf(TCP_Port);
     }
 
-
-    public void FSTrackerConnection() throws IOException, ClassNotFoundException{
-
-        Socket socket = new Socket(server_address, server_port);
-        
-        out = new ObjectOutputStream(socket.getOutputStream());
-        in = new ObjectInputStream(socket.getInputStream()); 
-
-        System.out.println("FS Track Protocol connection established with server " + server_address + " on port " + server_port);
-
-        register();
-        System.out.println("Node has been Registered in FS Tracker;\n");
-
-        Scanner scanner = new Scanner(System.in);
-
-        while(true){
-
-            System.out.println("Waiting for a command:");
-            String command = (scanner.nextLine()).toUpperCase();
-
-            switch(command){
-                
-                case "UPDATE":
-
-                    update();
-                    break;
-
-                case "GET":
-
-                    System.out.println("Choose file to locate:");
-                    String file_name = scanner.nextLine();
-                    get(file_name);
-
-                    byte[] received_packet = (byte[]) in.readObject();
-                    Protocol final_packet = Protocol.unpack(received_packet);
-
-                    if(final_packet.getFiles().isEmpty()) 
-                        System.out.println("Specified file could not be found in any registered Node;");
-
-                    else{
-                        for (String key : final_packet.getFiles().keySet()) {
-                            System.out.println(key + ": " + final_packet.getFiles().get(key) + " blocks");
-                        }
-                    }
-
-                    break;
-                
-                case "EXIT":
-
-                    exit();
-                    socket.close();
-                    return;
-
-                default:
-
-                    System.out.println("Error: command is not valid;");
-
-            }
-        }
-    }
-
-
-    public String getAddress() throws IOException{
+    public String getAddress() throws IOException {
 
         String address = InetAddress.getLocalHost().getHostAddress();// + ":" + String.valueOf(TCP_Port);
         return address;
     }
 
-    public void register() throws IOException{
+    public void register() throws IOException {
 
         String address = getAddress();
 
@@ -106,9 +48,11 @@ public class FS_Node {
         byte[] packet_ready = packet.packUp();
         out.writeObject(packet_ready);
         out.flush();
+
+        System.out.println("Node has been Registered in FS Tracker;\n");
     }
 
-    public void update() throws IOException{
+    public void update() throws IOException {
 
         String address = getAddress();
         this.files = readFilesToMap(directory);
@@ -120,76 +64,85 @@ public class FS_Node {
         out.flush();
     }
 
-    public void get(String file_name) throws IOException{
+    public void get(String file_name) throws IOException {
 
         String address = getAddress();
 
         Map<String, List<Integer>> files_just_name = new HashMap<>();
-        files_just_name.put(file_name, new ArrayList<Integer>()); 
-        Protocol packet = new Protocol("GET", address, files_just_name); 
+        files_just_name.put(file_name, new ArrayList<Integer>());
+        Protocol packet = new Protocol("GET", address, files_just_name);
 
         byte[] packet_ready = packet.packUp();
         out.writeObject(packet_ready);
         out.flush();
     }
 
-    public void exit() throws IOException{
-
+    /*
+     * public void exit() throws IOException {
+     * 
+     * String address = getAddress();
+     * 
+     * // Use a placeholder as there will be no need to send actual file information
+     * Map<String, List<Integer>> place_holder = new HashMap<>();
+     * place_holder.put("placeHolder", new ArrayList<>());
+     * 
+     * Protocol packet = new Protocol("EXIT", address, place_holder);
+     * byte[] packet_ready = packet.packUp();
+     * out.writeObject(packet_ready);
+     * out.flush();
+     * 
+     * in.close();
+     * out.close();
+     * }
+     */
+    public void exit() throws IOException {
         String address = getAddress();
 
-        // Use a placeholder as there will be no need to send actual file data
-        Map<String, List<Integer>> place_holder = new HashMap<>();
-        place_holder.put("placeHolder", new ArrayList<>());
+        // Use a placeholder as there will be no need to send actual file information
+        Map<String, List<Integer>> placeHolder = new HashMap<>();
+        placeHolder.put("placeHolder", new ArrayList<>());
 
-        Protocol packet = new Protocol("EXIT", address, place_holder);
-        byte[] packet_ready = packet.packUp();
-        out.writeObject(packet_ready);
+        Protocol packet = new Protocol("EXIT", address, placeHolder);
+        byte[] packetReady = packet.packUp();
+
+        // Send the exit packet over TCP
+        out.writeObject(packetReady);
         out.flush();
 
+        // Close TCP socket
+        tcp_socket.close();
+
+        // Close UDP socket (if it's open)
+        if (udp_socket != null && !udp_socket.isClosed()) {
+            udp_socket.close();
+        }
+
+        // Close input and output streams
         in.close();
         out.close();
     }
 
-    /* 
-    public Map<String, Integer> readFilesToMap(String directoryPath){    }
+    public class FileBlock {
 
+        private Integer Id;
+        private byte[] data;
 
-        File directory = new File(directoryPath);
-    
-        Map<String, Integer> fileBlockCounts = new HashMap<>();
-    
-        File[] files = directory.listFiles();
-    
-        if (files != null) {
-            for (File file : files) {
-                if (file.isFile()) {
-                    String fileName = file.getName();
-    
-                    try (FileInputStream fileInputStream = new FileInputStream(file)) {
-                        int blockCount = 0;
-                        int bufferSize = 100; 
-                        byte[] buffer = new byte[bufferSize];
-                        int bytesRead;
-    
-                        while ((bytesRead = fileInputStream.read(buffer)) != -1) {
-                            blockCount++;fileName    }
-
-                        }
-    
-                        fileBlockCounts.put(fileName, blockCount);
-    
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
+        public FileBlock(Integer blockId, byte[] data) {
+            this.Id = blockId;
+            this.data = data;
         }
-    
-        return fileBlockCounts;
+
+        public Integer getBlockId() {
+            return Id;
+        }
+
+        public byte[] getData() {
+            return data;
+        }
     }
-*/
 
     public Map<String, List<FileBlock>> readFilesToMap(String directoryPath) {
+
         File directory = new File(directoryPath);
         Map<String, List<FileBlock>> fileBlocksMap = new HashMap<>();
 
@@ -202,7 +155,7 @@ public class FS_Node {
 
                     try (FileInputStream fileInputStream = new FileInputStream(file)) {
                         int blockId = 0;
-                        int bufferSize = 100; 
+                        int bufferSize = 100;
                         byte[] buffer = new byte[bufferSize];
                         int bytesRead;
 
@@ -244,27 +197,99 @@ public class FS_Node {
         return blockIdsMap;
     }
 
-    public class FileBlock {
-        private Integer blockId;
-        private byte[] data;
-    
-        public FileBlock(Integer blockId, byte[] data) {
-            this.blockId = blockId;
-            this.data = data;
+    public void setupPeer() {
+
+        final int UDP_PORT = 9090;
+        final int BUFFER_SIZE = 1024;
+
+        try {
+            DatagramSocket udp_socket = new DatagramSocket(UDP_PORT);
+
+            System.out.println("Listening for new UDP connections on port " + UDP_PORT);
+
+            while (true) {
+
+                byte[] buffer = new byte[BUFFER_SIZE];
+                DatagramPacket packet = new DatagramPacket(buffer, BUFFER_SIZE);
+
+                udp_socket.receive(packet);
+
+                // Process the received data (e.g., handle the command or message)
+                String receivedData = new String(packet.getData(), 0, packet.getLength());
+                System.out.println("Received from " + packet.getAddress() + ": " + receivedData);
+
+                // You can spawn a new thread or handle the received data as needed
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-    
-        public Integer getBlockId() {
-            return blockId;
-        }
-    
-        public byte[] getData() {
-            return data;
+    }
+
+    public void setupTrackerConnection() throws IOException, ClassNotFoundException {
+
+        tcp_socket = new Socket(server_address, server_port);
+        out = new ObjectOutputStream(tcp_socket.getOutputStream());
+        in = new ObjectInputStream(tcp_socket.getInputStream());
+
+        System.out.println(
+                "FS Track Protocol connection established with server " + server_address + " on port " + server_port);
+
+        register();
+    }
+
+    public void commandHandler() throws IOException, ClassNotFoundException {
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+
+            System.out.println("Waiting for a command:");
+            String command = (scanner.nextLine()).toUpperCase();
+
+            switch (command) {
+
+                case "UPDATE":
+
+                    update();
+                    break;
+
+                case "GET":
+
+                    System.out.println("Choose file to locate:");
+                    String file_name = scanner.nextLine();
+                    get(file_name);
+
+                    byte[] received_packet = (byte[]) in.readObject();
+                    Protocol final_packet = Protocol.unpack(received_packet);
+
+                    if (final_packet.getFiles().isEmpty())
+                        System.out.println("Specified file could not be found in any registered Node;");
+
+                    else {
+                        for (String key : final_packet.getFiles().keySet()) {
+                            System.out.println(key + ": " + final_packet.getFiles().get(key) + " blocks");
+                        }
+                    }
+
+                    break;
+
+                case "EXIT":
+
+                    exit();
+                    // tcp_socket.close();
+                    return;
+
+                default:
+
+                    System.out.println("Error: command is not valid;");
+
+            }
         }
     }
 
     public static void main(String[] args) throws IOException, ClassNotFoundException {
 
-        if(args.length < 3){
+        if (args.length < 3) {
             System.out.println("Not enough arguments");
             return;
         }
@@ -274,6 +299,9 @@ public class FS_Node {
         int server_port = Integer.parseInt(args[2]);
 
         FS_Node node = new FS_Node(server_port, server_address, directory);
-        node.FSTrackerConnection();
+
+        node.setupTrackerConnection();
+        node.setupPeer();
+        node.commandHandler();
     }
 }
--- src/FS_Tracker.java
+++ src/FS_Tracker.java
@@ -3,17 +3,17 @@ import java.net.*;
 import java.io.*;
 
 public class FS_Tracker {
-    
+
     private Map<String, Map<String, List<Integer>>> fs_nodes;
     private int port;
 
-    public FS_Tracker(){
-        
+    public FS_Tracker() {
+
         this.fs_nodes = new HashMap<>();
         this.port = 9090;
     }
 
-    public void registerNode(Protocol infos){
+    public void registerNode(Protocol infos) {
 
         String node_address = infos.getNodeAddress();
         Map<String, List<Integer>> files = infos.getFiles();
@@ -22,33 +22,32 @@ public class FS_Tracker {
         System.out.println("Node " + node_address + " Registered;");
     }
 
-    public void updateNode(Protocol infos){
+    public void updateNode(Protocol infos) {
 
         String node_address = infos.getNodeAddress();
         Map<String, List<Integer>> files = infos.getFiles();
 
-        if(fs_nodes.containsKey(node_address)){
+        if (fs_nodes.containsKey(node_address)) {
 
             fs_nodes.put(node_address, files);
             System.out.println("Node " + node_address + " Updated;");
-        }
-        else{
+        } else {
             System.out.println("Specified Node (" + node_address + ") does not exist;");
         }
     }
 
-    public Map<String, List<Integer>> getNodes(Protocol infos){
+    public Map<String, List<Integer>> getNodes(Protocol infos) {
 
         Map<String, List<Integer>> files = infos.getFiles();
-      
-        // The sent files have a single key (file name we want to locate) with no blocks
+
+        // The sent files have a single key (file name we want to locate)
         String file_name = files.keySet().iterator().next();
 
         Map<String, List<Integer>> node_list = new HashMap<>();
 
         for (String key : fs_nodes.keySet()) {
 
-            if(fs_nodes.get(key).containsKey(file_name)){
+            if (fs_nodes.get(key).containsKey(file_name)) {
 
                 // Add node IP as key and blocks from said node as value
                 List<Integer> node_blocks = fs_nodes.get(key).get(file_name);
@@ -59,7 +58,7 @@ public class FS_Tracker {
         return node_list;
     }
 
-    public byte[] sendNodes(Map<String, List<Integer>> node_list) throws IOException{
+    public byte[] sendNodes(Map<String, List<Integer>> node_list) throws IOException {
 
         String address = InetAddress.getLocalHost().getHostAddress() + ":" + String.valueOf(port);
         Protocol packet = new Protocol("LIST", address, node_list);
@@ -69,7 +68,7 @@ public class FS_Tracker {
     }
 
     public void start() throws IOException {
-        
+
         ServerSocket serverSocket = new ServerSocket(port);
         System.out.println("FS_Tracker is running. Listening on port " + port);
 
@@ -78,21 +77,20 @@ public class FS_Tracker {
             new ServerThread(clientSocket).start();
         }
     }
-    
 
-    class ServerThread extends Thread{
+    class ServerThread extends Thread {
 
         private Socket socket;
         private ObjectInputStream in;
         private ObjectOutputStream out;
 
-        public ServerThread(Socket socket){
+        public ServerThread(Socket socket) {
 
             this.socket = socket;
         }
 
         @Override
-        public void run(){
+        public void run() {
 
             try {
 
@@ -100,25 +98,20 @@ public class FS_Tracker {
                 out = new ObjectOutputStream(socket.getOutputStream());
                 out.flush();
 
-                // tentar receber primeiro o tamanho para criar um byte[tamanho_do_packet];
-                //int packet_size = (int) in.readObject();
-                //byte[] received_packet = new byte[packet_size];
-                //byte received_packet = (byte[]) in.readObject();
-
-                while(true){
+                while (true) {
 
                     byte[] received_packet = (byte[]) in.readObject();
                     Protocol final_packet = Protocol.unpack(received_packet);
 
                     String command = final_packet.getCommand();
 
-                    if(command.toUpperCase().equals("REGISTER")){
+                    if (command.toUpperCase().equals("REGISTER")) {
 
                         registerNode(final_packet);
                         out.flush();
                     }
 
-                    else if(command.toUpperCase().equals("UPDATE")){
+                    else if (command.toUpperCase().equals("UPDATE")) {
 
                         updateNode(final_packet);
                         for (String key : final_packet.getFiles().keySet()) {
@@ -127,7 +120,7 @@ public class FS_Tracker {
                         out.flush();
                     }
 
-                    else if(command.toUpperCase().equals("GET")){
+                    else if (command.toUpperCase().equals("GET")) {
 
                         Map<String, List<Integer>> packet = getNodes(final_packet);
                         byte[] packet_ready = sendNodes(packet);
@@ -135,31 +128,28 @@ public class FS_Tracker {
                         out.flush();
                     }
 
-                    else if(command.toUpperCase().equals("EXIT")){
+                    else if (command.toUpperCase().equals("EXIT")) {
 
-                        System.out.println("Terminating connection with Node ( " + final_packet.getNodeAddress() + " );");
+                        System.out
+                                .println("Terminating connection with Node ( " + final_packet.getNodeAddress() + " );");
                         fs_nodes.remove(final_packet.getNodeAddress());
                         in.close();
                         out.close();
                         socket.close();
                         break;
                     }
-            
+
                 }
 
             } catch (IOException | ClassNotFoundException e) {
                 e.printStackTrace();
-            }        
+            }
         }
     }
-    
-    public static void main(String [] args) throws IOException{ // porque IOException?!
-        
+
+    public static void main(String[] args) throws IOException { // porque IOException?!
+
         FS_Tracker fs_Tracker = new FS_Tracker();
         fs_Tracker.start();
     }
-
 }
-
-
-
